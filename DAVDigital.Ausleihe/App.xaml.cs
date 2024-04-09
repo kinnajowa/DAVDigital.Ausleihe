515455c513b772bd1a24e34f920a84d89ec60bfb
Jannik Taubert <git@tbrt.eu>
2024-04-09 10:04:14 +0200

    Projektdateien hinzufügen.

---
+++ DAVDigital.Ausleihe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DAVDigital.Ausleihe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DAVDigital.Ausleihe/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DAVDigital.Ausleihe
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
